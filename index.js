e1da659cffe9cdecd424bba90e7f37b2047db47d
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2025-04-15 19:48:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const app = express()
 app.use(cors(
     {
         origin: "https://employee-management-frotend.vercel.app",
+     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
         credentials: true
     }
 ))
